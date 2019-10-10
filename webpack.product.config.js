bca5d2b4c0427ef6557cab1d21b7dd9bb9241eb2
zionHuang <hjszion@gmail.com>
2019-10-10 13:41:09 -0700

    update

---
+++ webpack.product.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'production',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [{
+            test: /\.(sc|c|sa)ss$/,  //正则
+            use: [
+                MiniCssExtractPlugin.loader,{
+                loader:'css-loader',
+                options:{
+                    sourceMap:true
+                }
+            }, {
+                loader: 'postcss-loader',
+                options: {
+                    ident: 'postcss',
+                    sourceMap: true,
+                    plugins: (loader) => [
+                        require('autoprefixer')() // 添加插件
+                    ]
+                }
+            }, {
+                loader: "sass-loader",
+                options: {
+                    sourceMap: true
+                }
+            }]   //从后往前去应用到模块上
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css', // 设置最终输出的文件名
+            chunkFilename: '[id].css'
+        })
+    ]
+};
