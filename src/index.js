685d873739ab3e56f97881c24b764dae45e497b0
zionHuang <hjszion@gmail.com>
2019-10-10 00:43:36 -0700

    added load style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';   //ES6 import 语法
+import './style/index.css';   //loader => css-loader module style-loader
 
 function createDomElement() {
   var dom = document.createElement('div');
-  dom.innerHTML = _.join(['aicoder', '.com', ' wow'], '');
+  dom.innerHTML = _.join(['google', '.com', ' wow'], '');
+  dom.className = 'box';
+//dom.classList.add('box');
   return dom;
 }
 
